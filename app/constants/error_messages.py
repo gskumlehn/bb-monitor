b7a9340774ec139b887d5e7d9a3cdd990fc23745
Gabriel Kumlehn <gskumlehn@gmail.com>
2025-10-20 16:55:18 -0300

    Cria mensagens de erro

---
+++ app/constants/error_messages.py
@@ -0,0 +1,10 @@
+class ErrorMessages:
+    enum = {
+        "DirectorateCode.invalid": "Diretoria inválida",
+    }
+
+    model = {
+        "Mailing.email.empty": "Email não pode ser vazio.",
+        "Mailing.email.invalid": "Formato do email é inválido.",
+        "Mailing.email.domain.invalid": "Domínio do email não é permitido",
+    }
