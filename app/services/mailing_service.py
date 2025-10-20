d99fa8dd64013f0bf5a529a6b1cd86aac3f516da
Gabriel Kumlehn <gskumlehn@gmail.com>
2025-10-20 15:53:06 -0300

    Atualiza service

--- app/services/mailing_service.py
+++ app/services/mailing_service.py
@@ -1,12 +1,33 @@
-from typing import Iterable, List
+from typing import Iterable, List, Union, Tuple
 from app.repositories.mailing_repository import MailingRepository
+from app.enums.directorate_codes import DirectorateCode
+from app.constants.mailing_constants import MailingConstants
+from app.utils.email_utils import EmailUtils
 
 class MailingService:
-    def save(self, email: str, directorate_code: str) -> None:
-        MailingRepository.save(email, directorate_code)
+
+    def save(self, email: str, directorate_code: Union[str, DirectorateCode]) -> None:
+        email_norm, directorate_code = self.validate_save(email, directorate_code)
+        MailingRepository.save(email_norm, directorate_code)
+
+    def validate_save(self, email: str, directorate_code: Union[str, DirectorateCode]) -> Tuple[str, DirectorateCode]:
+        email_norm = self.validate_email(email)
+        directorate_code = DirectorateCode.from_str(directorate_code)
+
+        return email_norm, directorate_code
+
+    def validate_email(self, email: str) -> str:
+        email_norm = EmailUtils.normalize_email(email)
+        if not email_norm:
+            raise ValueError("email não pode ser vazio")
+        if not EmailUtils.is_valid_email(email_norm):
+            raise ValueError("email inválido")
+        if not EmailUtils.is_allowed_domain(email_norm, MailingConstants.ALLOWED_DOMAINS):
+            raise ValueError("domínio inválido: apenas @futurebrand.com ou @futurebrand.com.br são permitidos")
+        return email_norm
 
     def delete(self, email: str, directorate_code: str) -> int:
-        return MailingRepository.delete(email, directorate_code)
+        return MailingRepository.delete(EmailUtils.normalize_email(email), DirectorateCode.from_str(directorate_code))
 
-    def get_emails_by_directorates(self, codes: Iterable[str]) -> List[str]:
+    def get_emails_by_directorates(self, codes: Iterable[DirectorateCode]) -> List[str]:
         return MailingRepository.get_emails_by_directorates(codes)
