f5839da958b046da662b8553f9954da189a13103
Gabriel Kumlehn <gskumlehn@gmail.com>
2025-10-20 15:51:47 -0300

    Altera model de mailing para ter enum

--- app/models/mailing.py
+++ app/models/mailing.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Enum as SAEnum
+from app.enums.directorate_codes import DirectorateCode
 
 Base = declarative_base()
 
@@ -8,5 +9,7 @@ class Mailing(Base):
     __table_args__ = {"schema": "bb_monitor"}
 
     email = Column(String, primary_key=True)
-    directorate_code = Column(String, primary_key=True)
-
+    directorate_code = Column(
+        SAEnum(DirectorateCode, values_callable=lambda enum: [e.name for e in enum], native_enum=False),
+        primary_key=True
+    )
