3987294d512337c80565d0ee59fd100d315bd441
Gabriel Kumlehn <gskumlehn@gmail.com>
2025-10-21 19:06:47 -0300

    Cria controller para a ingestão

---
+++ app/controllers/ingestions_controller.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, jsonify
+from app.services.ingestion_service import IngestionService
+
+ingestion_bp = Blueprint("ingestion", __name__)
+
+@ingestion_bp.route("/ingest", methods=["POST"])
+def ingest():
+    try:
+        ingestion_service = IngestionService()
+        alerts = ingestion_service.ingest()
+        return jsonify({"message": "Ingestão concluída com sucesso.", "alerts": [alert.id for alert in alerts]}), 200
+    except Exception as e:
+        return jsonify({"error": "Erro ao executar a ingestão.", "details": str(e)}), 500
+
