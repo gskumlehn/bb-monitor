efc747c02b6720d4b968f75fd1d75073f4995d49
Gabriel Kumlehn <gskumlehn@gmail.com>
2025-10-21 15:38:07 -0300

    Renomeia métodos

--- app/enums/alert_type.py
+++ app/enums/alert_type.py
@@ -4,14 +4,15 @@ class AlertType(EnumInterface):
     PRESS = "Imprensa"
     SOCIAL_MEDIA = "Rede Social"
 
-    @classmethod
-    def csv_to_type_list_by_values(cls, csv: str):
-        return [cls(value) for value in csv.split(",") if value]
+    @staticmethod
+    def values_csv_to_type_list(csv: str):
+        return [AlertType(value) for value in csv.split(",") if value]
 
-    @classmethod
-    def csv_to_type_list_by_names(cls, csv: str):
-        return [cls.from_name(name) for name in csv.split(",") if name]
+    @staticmethod
+    def names_csv_to_type_list(csv: str):
+        return [AlertType.from_name(name) for name in csv.split(",") if name]
 
-    @classmethod
-    def type_list_to_csv_by_values(cls, types: list):
+    @staticmethod
+    def type_list_to_values_csv(types: list):
         return ",".join([type_.value for type_ in types])
+
