22543ea8bb6238fab7255f417ba5c397a9dceb1a
Gabriel Kumlehn <gskumlehn@gmail.com>
2025-10-21 19:01:38 -0300

    Add subtract days

--- app/utils/date_utils.py
+++ app/utils/date_utils.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
 
 class DateUtils:
@@ -7,10 +7,13 @@ class DateUtils:
 
     @staticmethod
     def to_iso_format(dt: datetime, tz: str = BRAZIL_TZ) -> str:
-        return dt.astimezone(ZoneInfo(tz)).strftime("%Y-%m-%dT%H:%M:%SZ")
+        return dt.astimezone(ZoneInfo(tz)).strftime("%Y-%m-%dT%H:%M:%S%z")
 
     @staticmethod
     def from_date_and_time(date_str: str, time_str: str, tz: str = BRAZIL_TZ) -> datetime:
         dt = datetime.strptime(f"{date_str} {time_str}", "%d/%m/%Y %H:%M")
         return dt.replace(tzinfo=ZoneInfo(tz))
 
+    @staticmethod
+    def subtract_days(base_datetime: datetime, days: int) -> datetime:
+        return base_datetime - timedelta(days=days)
