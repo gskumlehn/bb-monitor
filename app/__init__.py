5067d78602f74eed7603ebbfd229c66184a3ae20
Gabriel Kumlehn <gskumlehn@gmail.com>
2025-10-24 11:57:12 -0300

    Add config de formatacao de textos jsons

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,7 @@ def create_app():
 
     app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1, x_prefix=1)
     app.config["PREFERRED_URL_SCHEME"] = "https"
+    app.config['JSON_AS_ASCII'] = False
 
     app.register_blueprint(mailing_bp, url_prefix="/mailing")
     app.register_blueprint(root_bp, url_prefix="/")
