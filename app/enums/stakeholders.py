3c8dba552b281c854b74e32c809fcd6468fd8320
Gabriel Kumlehn <gskumlehn@gmail.com>
2025-10-27 14:13:18 -0300

    Cria novos enums

---
+++ app/enums/stakeholders.py
@@ -0,0 +1,8 @@
+from app.interfaces.enum_interface import EnumInterface
+
+class Stakeholders(EnumInterface):
+    PRESS_JOURNALISTS = "Imprensa/Jornalistas"
+    CLIENT_SOCIETY = "Cliente/Sociedade"
+    POLITICAL_ACTORS_PUBLIC_AGENCIES = "Atores políticos/Órgãos públicos"
+    UNIONS_EMPLOYEES = "Sindicatos/Funcionários"
+
