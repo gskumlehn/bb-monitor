0f656ff3fb0813ddf9046d15dd8dc2273e4dd37a
Gabriel Kumlehn <gskumlehn@gmail.com>
2025-10-27 14:13:32 -0300

    Passa método de enum para interface

--- app/enums/alert_type.py
+++ app/enums/alert_type.py
@@ -3,7 +3,3 @@ from app.interfaces.enum_interface import EnumInterface
 class AlertType(EnumInterface):
     PRESS = "Imprensa"
     SOCIAL_MEDIA = "Rede Social"
-
-    @staticmethod
-    def values_csv_to_type_list(csv: str):
-        return [AlertType(value.strip()) for value in csv.split(",") if value.strip()]
--- app/interfaces/enum_interface.py
+++ app/interfaces/enum_interface.py
@@ -9,3 +9,9 @@ class EnumInterface(Enum):
             return cls[name]
         except KeyError:
             raise ValueError(ErrorMessages.enum[f"{cls.__name__}.invalid"])
+
+    @classmethod
+    def values_csv_to_type_list(cls, csv: str):
+        if not csv:
+            return []
+        return [cls(value.strip()) for value in csv.split(",") if value.strip()]
