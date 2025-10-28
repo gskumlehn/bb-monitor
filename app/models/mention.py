f11e0be85e0f3181d26cd060ae02b9a25d6de1f2
Gabriel Kumlehn <gskumlehn@gmail.com>
2025-10-28 12:48:44 -0300

    Cria fluxo de save de mention a aprtir da ingestão

--- app/models/mention.py
+++ app/models/mention.py
@@ -7,6 +7,6 @@ class Mention(Base):
     __tablename__ = "mention"
     __table_args__ = {"schema": "bb_monitor"}
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(String(64), primary_key=True)
     alert_id = Column(String(64), ForeignKey("bb_monitor.alert.id"), nullable=False)
     url = Column(Text, nullable=False)
