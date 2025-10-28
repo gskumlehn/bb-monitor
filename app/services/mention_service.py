a4eec85e29d43cbeb790641ba155c423088c917d
Gabriel Kumlehn <gskumlehn@gmail.com>
2025-10-28 12:33:43 -0300

    Cria fluxo de save de mention

--- app/services/mention_service.py
+++ app/services/mention_service.py
@@ -1,18 +1,32 @@
 from typing import Dict, Any, List
 from app.models.mention import Mention
+from app.repositories.mention_repository import MentionRepository
+from app.services.brandwatch_service import BrandwatchService
+from app.models.alert import Alert
 
 class MentionService:
 
-    def create_mention(self, data: Dict[str, Any]) -> Mention:
-        return Mention(
-            url=data.get("url"),
-            original_url=data.get("originalUrl"),
-            thread_url=data.get("threadURL"),
-            title=data.get("title"),
-            content=data.get("content"),
-            author=data.get("author"),
-            published_date=data.get("publishedDate"),
-            sentiment=data.get("sentiment"),
-            tags=data.get("tags", []),
+    def save(self, alert: Alert) -> List[Mention]:
+        brandwatch_service = BrandwatchService()
+
+        mentions_data = brandwatch_service.fetch_mentions_by_urls_with_retry(
+            urls=alert.urls,
+            end_date=alert.delivery_datetime
         )
 
+        for mention_data in mentions_data:
+            mention = self.create({
+                "alert_id": alert.id,
+                "id": mention_data.get("brandwatch_id"),
+                "url": mention_data["url"],
+            })
+
+            MentionRepository.save(mention)
+
+
+    def create(self, data: Dict[str, Any]) -> Mention:
+        return Mention(
+            alert_id=data.get("alert_id"),
+            url=data.get("url"),
+            created_at=data.get("created_at")
+        )
