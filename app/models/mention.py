3ad77d325195f0e866ff219f90084e242d846208
Gabriel Kumlehn <gskumlehn@gmail.com>
2025-10-28 14:51:41 -0300

    Corrige model

--- app/models/mention.py
+++ app/models/mention.py
@@ -7,6 +7,6 @@ class Mention(Base):
     __tablename__ = "mention"
     __table_args__ = {"schema": "bb_monitor"}
 
-    id = Column(String(64), primary_key=True)
-    alert_id = Column(String(64), ForeignKey("bb_monitor.alert.id"), nullable=False)
-    url = Column(Text, nullable=False)
+    id = Column(String(64), nullable=True)
+    alert_id = Column(String(64), nullable=False)
+    url = Column(Text, primary_key=True, nullable=False)
