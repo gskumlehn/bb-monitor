a6e546374dc986706727a8647ec12b43404f79a2
Gabriel Kumlehn <gskumlehn@gmail.com>
2025-10-30 16:57:47 -0300

    Corrige

--- app/enums/press_source.py
+++ app/enums/press_source.py
@@ -4,7 +4,7 @@ class PressSource(EnumInterface):
     ONE_RELEVANT_VEHICLE = ">= 1 Veículo Relevante"
     THREE_RELEVANT_VEHICLES = "+3 Veículos Relevantes"
     GROUP_B = "Grupo B (15 à 50 milhões de acesso/mês)"
-    GROUP_A = "Grupo A (acima de 50 milhões de acesso/mês)"
+    GROUP_A = "Grupo A (acima de 50 milhões de acesso/mês"
     MENTIONS_BB_IN_TITLE = "Cita BB no título"
     MENTIONS_BB_IN_SUBTITLE = "Cita BB no subtítulo"
     BB_IMAGE_OR_VIDEO = "Imagem/ Vídeo do BB"
