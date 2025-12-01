918bb19dddf4116852cb1a155ef4956eebf9cf7b
Gabriel Kumlehn <gskumlehn@gmail.com>
2025-12-01 12:09:50 -0300

    Cria endpoint de listagem de diretorias para alert

--- app/controllers/directorate_controller.py
+++ app/controllers/directorate_controller.py
@@ -1,11 +1,12 @@
-from flask import abort, Blueprint, render_template
-
 from app.enums.directorate_codes import DirectorateCode
 from app.services.alert_service import AlertService
+from app.services.directorate_service import DirectorateService
+from flask import abort, Blueprint, render_template, jsonify
 
 directorate_bp = Blueprint("directorate", __name__, url_prefix="/directorate")
 
 alert_service = AlertService()
+directorate_service = DirectorateService()
 
 @directorate_bp.route("/alert/<alert_id>", methods=["GET"])
 def manage_directorates(alert_id):
@@ -18,3 +19,12 @@ def manage_directorates(alert_id):
         alert=alert,
         directorate_codes=DirectorateCode.list_excluding()
     )
+
+@directorate_bp.route("/list_alerted_directorates/<alert_id>", methods=["POST"])
+def list_alerted_directorates(alert_id):
+    alert = alert_service.get_by_id(alert_id)
+    if not alert:
+        abort(404, description="Alerta não encontrado")
+
+    result = directorate_service.list_categories_for_urls(alert)
+    return jsonify(result), 200
