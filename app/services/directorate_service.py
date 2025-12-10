5173b1a368c68dc31d918bd34a4107a55fbc36d6
Gabriel Kumlehn <gskumlehn@gmail.com>
2025-12-10 17:45:20 -0300

    Altera chamada de diretorias listadas para subcategorias do alert

--- app/services/directorate_service.py
+++ app/services/directorate_service.py
@@ -1,14 +1,9 @@
-from app.repositories.mention_repository import MentionRepository
 from app.constants.mailing_constants import MailingConstants
 from app.enums.directorate_codes import DirectorateCode
 
 class DirectorateService:
 
-    def list_categories_for_urls(self, alert):
-        category_names = MentionRepository.list_categories_for_urls(alert.urls)
-        return self.get_directorates_by_categories(category_names)
-
-    def get_directorates_by_categories(self, category_names: list[str]) -> list[str]:
+    def get_directorates_by_subcategories(self, category_names: list[str]) -> list[str]:
         category_to_directorate_map = MailingConstants.CATEGORY_TO_DIRECTORATE_MAP
         directorates = {DirectorateCode.DIRIS, DirectorateCode.DICOI, DirectorateCode.OUVIDORIA}
 
