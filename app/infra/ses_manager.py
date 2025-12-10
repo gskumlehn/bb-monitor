445563d7c3e24bf5d5c5b4cbacb386929a4a734c
Gabriel Kumlehn <gskumlehn@gmail.com>
2025-12-10 11:42:57 -0300

    remove force

--- app/infra/ses_manager.py
+++ app/infra/ses_manager.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-AWS_REGION = os.getenv("AWS_REGION", "us-east-1")
+AWS_REGION = os.getenv("AWS_REGION")
 SES_SENDER_EMAIL = os.getenv("SES_SENDER_EMAIL")
 
 class SESManager:
